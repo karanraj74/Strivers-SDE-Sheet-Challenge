45f46ef0a46450866988b09112de223f9db890af
karanraj74 <115173413+karanraj74@users.noreply.github.com>
2023-07-21 09:43:43 +0530

    Create 	Symmetric Binary Tree.cpp

---
+++ Binary Tree part-III/Symmetric Binary Tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool solve(TreeNode* l,TreeNode* r){
+         if(l == NULL && r == NULL){
+            return true;
+        }
+         if(l == NULL || r == NULL){
+            return false;
+        }
+        if(l->val!=r->val){
+            return false;
+        }
+        return solve(l->left,r->right) && solve(l->right,r->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL){
+            return true;
+        }
+        return solve(root->left,root->right);
+        // return isSymmetric(root->left) && isSymmetric(root->right);
+    }
+};
